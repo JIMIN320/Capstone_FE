c2595eadb9d2003fe7cd6060b684d05633aeb09a
ty9249 <https://github.com/ty9249>
2024-04-16 00:31:13 +0900

    추가 수정

--- app/src/main/java/com/example/whenandwhere/GrouplistActivity.kt
+++ app/src/main/java/com/example/whenandwhere/GrouplistActivity.kt
@@ -20,29 +20,18 @@ class GrouplistActivity : AppCompatActivity() {
         binding = ActivityGrouplistBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val spanCount = 1// 가로 아이템 수
-//        val horizontalSpacing = resources.getDimensionPixelSize(R.dimen.horizontal_spacing)
-//        val verticalSpacing = resources.getDimensionPixelSize(R.dimen.vertical_spacing)
+        val grouplist = arrayListOf(
+            Groups("하이파이브", "공적인 모임"),
+            Groups("뭐시기", "어쩌구"),
+        )
 
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-
-        val adapter = GroupAdapter(this@GrouplistActivity, groups) // 수정된 부분
-        binding.recyclerView.adapter = adapter
+        binding.recyclerView.adapter = GroupAdapter(this, grouplist) // 데이터 전달
 
         val makeBtn = findViewById<Button>(R.id.makemoim)
         makeBtn.setOnClickListener {
             val intent = Intent(this, NewGroupMake::class.java)
             startActivityForResult(intent, ADD_GROUP_REQUEST)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == ADD_GROUP_REQUEST && resultCode == Activity.RESULT_OK) {
-            val groupName = data?.getStringExtra("groupName")
-            val groupTheme = data?.getStringExtra("groupTheme")
-            groups.add(Groups(groupName, groupTheme))
-            binding.recyclerView.adapter?.notifyDataSetChanged()
         }
     }
 }
--- app/src/main/java/com/example/whenandwhere/middleplace.kt
+++ app/src/main/java/com/example/whenandwhere/middleplace.kt
@@ -2,10 +2,36 @@ package com.example.whenandwhere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.RadioButton
+import androidx.appcompat.app.AlertDialog
+import com.example.whenandwhere.databinding.ActivityMiddleplaceBinding
 
 class middleplace : AppCompatActivity() {
+    private lateinit var binding: ActivityMiddleplaceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_middleplace)
+
+        binding = ActivityMiddleplaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.finish.setOnClickListener{
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.alcohol,null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+
+            mBuilder.show()
+
+            val yesBtn = mDialogView.findViewById<RadioButton>(R.id.radioButton5)
+            yesBtn.setOnClickListener {
+
+            }
+
+            val noBtn = mDialogView.findViewById<RadioButton>(R.id.radioButton6)
+            noBtn.setOnClickListener {
+
+            }
+        }
     }
 }
