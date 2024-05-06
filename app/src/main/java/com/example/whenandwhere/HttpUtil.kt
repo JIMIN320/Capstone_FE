1c7f208948da9c34c1c0f81cd8c638a6f8220f30
Redpanda6801 <redpanda6801@gmail.com>
2024-05-06 14:33:42 +0900

    [Redpanda/05.06] 응답 시간 설정

--- app/src/main/java/com/example/whenandwhere/HttpUtil.kt
+++ app/src/main/java/com/example/whenandwhere/HttpUtil.kt
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
-val URL = "http://3.36.50.106:8080"
+val URL = "http://43.201.72.114:8080"
 
 class HttpUtil {
     // JWT를 SharedPreference에서 가져오는 함수
@@ -42,15 +43,25 @@ class HttpUtil {
                     chain.proceed(originalRequest)
                 }
             }
-        }.build()
+        }.
+        connectTimeout(100, TimeUnit.SECONDS) // 연결 시간 제한을 30초로 설정
+        .readTimeout(100, TimeUnit.SECONDS) // 읽기 시간 제한을 30초로 설정
+        .build()
 
         return client
     }
 
     fun createRetrofit() : Retrofit{
+        // 빈 헤더 추가
+        val client = OkHttpClient.Builder()
+            .connectTimeout(100, TimeUnit.SECONDS) // 연결 시간 제한을 30초로 설정
+            .readTimeout(100, TimeUnit.SECONDS) // 읽기 시간 제한을 30초로 설정
+            .build()
+
         // Retrofit 객체 생성
         val retrofit = Retrofit.Builder()
             .baseUrl(URL) // 로컬 서버의 IP 주소와 포트를 지정합니다.
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
@@ -69,9 +80,16 @@ class HttpUtil {
             return retrofit
         }
         else{
+            // 빈 헤더 추가
+            val client = OkHttpClient.Builder()
+                .connectTimeout(100, TimeUnit.SECONDS) // 연결 시간 제한을 30초로 설정
+                .readTimeout(100, TimeUnit.SECONDS) // 읽기 시간 제한을 30초로 설정
+                .build()
+
             // Retrofit 객체 생성
             retrofit = Retrofit.Builder()
                 .baseUrl(URL) // 로컬 서버의 IP 주소와 포트를 지정합니다.
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
